aeaf79c44c3927c6d3bd8b4d156509fc30a01b87
gizemakkus <gizemakkus@std.sehir.edu.tr>
2014-12-28 14:53:51 +0200

    Final Project Demo

---
+++ test.py
@@ -0,0 +1,13 @@
+
+__author__ = 'gizemakkus'
+#564260907 = user id
+
+
+
+
+from InstaGzm import *
+
+
+test=Insta("564260907.9b91f46.49dd44dae52c45dbade22c988131b096")
+test.unfollow_people_who_not_follow(564260907)
+test.follow_people_from_account(“username”,120):
